f4f812bf545afbddf16fa3adf95e063c25bb1c22
Warbler14 <firefly11a@naver.com>
2015-06-30 05:20:40 +0900

    test

---
+++ exp01.py
@@ -0,0 +1,14 @@
+__author__ = 'insung'
+
+x = -100;
+if x > 0 :
+    print ('Wow, this is elegant')
+else:
+    print ( 'Organization is the key')
+
+
+print("git update test")
+
+print("check")
+
+
---
+++ hello.py
@@ -0,0 +1,4 @@
+__author__ = 'insung'
+
+print("hello world!");
+print("hola mundo!");
