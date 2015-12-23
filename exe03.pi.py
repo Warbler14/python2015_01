042471c62b0712826be51c5a7eceae370c88e0f1
insung <firefly11b@gmail.com>
2015-12-23 22:30:51 +0900

    변수 선언

---
+++ exe03.pi.py
@@ -0,0 +1,27 @@
+__author__ = 'insung'
+
+def add(a,b):
+    return a+b
+
+print add(1,2)
+
+a , b = 01, 02
+c, d = 3, 4
+x = y = z = 0
+e = 3.5; f = 5.6
+print a,   b,   c,   d,   e,   f
+
+a = 2
+
+a *= 2+3
+
+print a
+
+a = eval( 'a + a + 10')
+
+print a
+
+a = 5
+exec 'a = a + 4'
+print a
+
