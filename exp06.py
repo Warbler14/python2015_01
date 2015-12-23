67bebe492343aa47e79a24c86afc636dcc80239f
insung <firefly11b@gmail.com>
2015-12-23 23:56:17 +0900

    사전

---
+++ exp06.py
@@ -0,0 +1,26 @@
+# dic
+
+dic = {'one':'haha', 'two':'hoho', 'three':'hihi'}
+print dic['one']
+
+# add 'four' : 'kiki'
+dic['four'] = 'kiki'
+print dic
+dic['one'] = 1
+print dic
+
+print 'two' in dic
+
+print dic.keys()
+key = dic.keys()
+
+print key[0]
+
+print dic.values()
+value = dic.values()
+
+print value[0]
+
+list1 = dic.items()
+
+print list1
