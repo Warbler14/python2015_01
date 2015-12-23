14c79b525ab6607cd472762c5a974c6e761b3623
insung <firefly11b@gmail.com>
2015-12-23 23:15:24 +0900

    키보드 입력

---
+++ exp04.py
@@ -0,0 +1,32 @@
+import pprint
+name = raw_input('insert your name : ')
+
+print 'your name is ' + name
+
+
+k = int( raw_input('insert number : '))
+
+print 'number is '
+print k
+
+
+i = input('int : ')
+print i
+
+
+k = input('expr:')
+print k
+
+k = input('expr:')
+print k
+
+print 1,; print 2
+
+
+complicated = [ 'spam', (1,2,3), ('ham', 'egg', ('ab', 'cd', ('abc', 'def')))]
+print complicated
+
+complicated = complicated * 2
+
+print complicated
+
