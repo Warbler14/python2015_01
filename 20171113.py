5fc1101b4c9a8e0357c32f18c3faf1caa2e13b2e
firefly11a@naver.com <firefly11a@naver.com>
2017-11-20 19:31:33 +0900

    20171120

---
+++ 20171113.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# 테스트 2017-11-13
+
+numberData = input("input number : ")
+
+print(str(numberData))
+
+strData = raw_input("input string : ")
+
+print("Echo : %s" % strData)
+
+
+
